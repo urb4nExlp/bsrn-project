61b8d43162e83f887ef88e8929528dfc3cbfe3ab
RobinGitHub24 <robin.schaefer1@outlook.de>
2024-05-05 20:34:16 +0200

    Update BSRNproject.py

--- BSRNproject.py
+++ BSRNproject.py
@@ -2,4 +2,4 @@ print("endlich geschafft!!!!!!!!")
 print("sieht das jemand, klapppt da, wenn ja schreibt mal n unsere Gruppe ein Hai111")
 print("Es funktiniert docjh")
 print("Ich werde noch verzweifeln weil ich nicht vertshee woher die fehlermeldung kommt bzw was icjh dagegen tun kann,"
-      "aber ich lassse mich nicht unterkiregen")
+      "aber ich lassse mich nicht unterkiregenprint("HAAAAAAAAAAAAALlllo")
