060f6c89a3c45b39ec9357a9d2480ea0d3758c15
urbanbenedikt00 <planet_beni@hotmail.com>
2024-05-12 16:20:22 +0200

    Update

--- bingogame.py
+++ bingogame.py
@@ -1,41 +1,40 @@
 import sys
 import posix_ipc
-import random
 
 
-class BingoGame:
-    def __init__(self):
-        pass
-    def start_host(self):
-        # Erstellen der Message Queue
-        mq_name = "/my_message_queue"
-        mq = posix_ipc.MessageQueue(mq_name, posix_ipc.O_CREAT)
+def start_host():
+    # Erstellen der Message Queue
+    mq_name = "/my_message_queue"
+    mq = posix_ipc.MessageQueue(mq_name, posix_ipc.O_CREAT)
 
-        print("Host gestartet. Warte auf Client...")
+    print("Host gestartet. Warte auf mind. 1 anderen Spieler...")
 
-        # Warten auf Nachricht vom Client
-        message, _ = mq.receive()
-        print(f"Nachricht vom Client erhalten: {message.decode()}")
+    # Warten auf Nachricht vom Client
+    message, _ = mq.receive()
+    print(f"Nachricht vom Client erhalten: {message.decode()}")
 
-        # Nachricht "Hallo Welt" ausgeben
-        print("Hallo Welt")
+    # Nachricht "Hallo Welt" ausgeben
+    print("Hallo Welt")
 
-        # Message Queue schließen
-        mq.close()
-        posix_ipc.unlink_shared_memory(mq_name)
+    # Message Queue schließen
+    mq.close()
+    posix_ipc.unlink_shared_memory(mq_name)
 
 
-    def win_client(self):
-        # Öffnen der existierenden Message Queue
-        mq_name = "/my_message_queue"
-        mq = posix_ipc.MessageQueue(mq_name)
+def win_client():
+    # Öffnen der existierenden Message Queue
+    mq_name = "/my_message_queue"
+    mq = posix_ipc.MessageQueue(mq_name)
 
-        # Nachricht an den Host senden
-        message = "Client gestartet"
-        mq.send(message.encode())
+    name = input("Spielernamen eingeben: ")
+    # Nachricht an den Host senden
+    message = "Spieler beigetreten: " + name
+    mq.send(message.encode())
 
-        # Message Queue schließen
-        mq.close()
+    # Message Queue schließen
+    mq.close()
+
+
 def ratespiel():
     zahl = 5
     print("Versuche Zahl 1-10 zu erraten:")
@@ -43,23 +42,18 @@ def ratespiel():
     while True:
         eingabe = input("Tipp:")
         zahl2 = int(eingabe)
-        if  zahl ==  zahl2:
+        if zahl == zahl2:
             print("Erraten!")
-            bingogame.win_client()
+            win_client()
             break
         else:
             print("Versuche es nochmal!")
+
 
 def create_round_file(filename):
     # open öffnet die Datei im 'w' Schreibmodus, ist keine Datei vorhanden wird sie erstellt
     with open(filename, 'w') as file:
         pass
-
-bingogame = BingoGame()
-
-
-
-
 
 
 if __name__ == "__main__":
@@ -68,7 +62,7 @@ if __name__ == "__main__":
         sys.exit(1)
 
     if sys.argv[1] == "-newround":
-        bingogame.start_host()
+        start_host()
     elif sys.argv[1] == "-joinround":
         ratespiel()
     elif sys.argv[1] == "-roundfile":
