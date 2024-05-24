25c0a39a8680e224ba719f8f6fae7d2b4318cfbd
bilalboukayoua <bilal.boukayoua@stud.fra-uas.de>
2024-05-24 17:43:03 +0200

    createlogfile (methode)

--- Dateizugriffe/LogFile.py
+++ Dateizugriffe/LogFile.py
@@ -2,4 +2,6 @@ def createlogfile(spielername):
     """ Erstellt eine neue Logdatei für den angegebenen Spieler."""
     logfile_name = f"{spielername}_log.txt"
     with open(logfile_name, 'w') as logfile:
-        
+        logfile.write(f"Logdatei für Spieler: {spielername}\n")
+        logfile.write(f"{'-' * 30}\n")
+    print(f"Die Logdatei '{logfile_name}' wurde erfolgreich erstellt.")
