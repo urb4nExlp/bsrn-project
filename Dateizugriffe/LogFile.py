a3edc66558049326d7a069314e24a3eb1b77c535
bilalboukayoua <bilal.boukayoua@stud.fra-uas.de>
2024-05-24 17:39:48 +0200

    .

--- Dateizugriffe/LogFile.py
+++ Dateizugriffe/LogFile.py
@@ -1,3 +1,5 @@
 def createlogfile(spielername):
     """ Erstellt eine neue Logdatei für den angegebenen Spieler."""
     logfile_name = f"{spielername}_log.txt"
+    with open(logfile_name, 'w') as logfile:
+        
