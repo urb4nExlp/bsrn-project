68d516314967c6c2d929a0df3761d32f07166266
bilalboukayoua <bilal.boukayoua@stud.fra-uas.de>
2024-05-25 16:45:28 +0200

    Logfile Methoden

--- Dateizugriffe/LogFile.py
+++ Dateizugriffe/LogFile.py
@@ -1,5 +1,5 @@
 def createlogfile(spielername):
-    """ Erstellt eine neue Logdatei für den angegebenen Spieler."""
+   # Erstellt eine neue Logdatei für den angegebenen Spieler.
     logfile_name = f"{spielername}_log.txt"
     with open(logfile_name, 'w') as logfile:
         logfile.write(f"Logdatei für Spieler: {spielername}\n")
@@ -7,4 +7,13 @@ def createlogfile(spielername):
     print(f"Die Logdatei '{logfile_name}' wurde erfolgreich erstellt.")
 
 def addtolog(spielername, eintrag):
-    """Fügt einen neuen Eintrag zur Logdatei des angegebenen Spielers hinzu."""
+    #Fügt einen neuen Eintrag zur Logdatei des angegebenen Spielers hinzu.
+ logfile_name = f"{spielername}_log.txt"
+if os.path.exists(logfile_name):
+    with open(logfile_name, 'a') as logfile:
+        logfile.write(f"{eintrag}\n")
+    print(f"Der Eintrag '{eintrag}' wurde zur Logdatei '{logfile_name}' hinzugefügt.")
+else:
+    print(f"Die Logdatei '{logfile_name}' existiert nicht. Bitte erst die Logdatei mit createlogfile erstellen.")
+
+    #...........................................
