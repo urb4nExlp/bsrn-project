4a8d6d1494d9aa950000d064d01c6ba12edad2b1
bilalboukayoua <bilal.boukayoua@stud.fra-uas.de>
2024-06-01 13:41:58 +0200

    Update LogFile.py

--- Dateizugriffe/LogFile.py
+++ Dateizugriffe/LogFile.py
@@ -1,5 +1,5 @@
 def createlogfile(spielername):
-   # Erstellt eine neue Logdatei für den angegebenen Spieler .
+   # Erstellt eine neue Logdatei für den angegebenen Spieler.
     logfile_name = f"{spielername}_log.txt"
     with open(logfile_name, 'w') as logfile:
         logfile.write(f"Logdatei für Spieler: {spielername}\n")
