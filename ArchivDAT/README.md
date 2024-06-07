ecb6e0c0211084c97757bad29d142a1b60b0b731
urb4nExlp <planet_beni@hotmail.com>
2024-06-07 11:52:20 +0200

    s

---
+++ ArchivDAT/README.md
@@ -0,0 +1,1 @@
+Erste Schritte in LATEX. Gutes Video: https://www.youtube.com/watch?v=GJyGdImiqBg
---
+++ BingoVisual/CursesStart.py
@@ -0,0 +1,14 @@
+import curses
+from curses import wrapper
+
+def main(stdscr):
+    stdscr.clear()
+    stdscr.addstr(3, 1, "Welcome to BingoVisual", curses.A_REVERSE)
+    stdscr.addstr(3, 30, "press x to finish")
+    stdscr.refresh()
+    while True:
+        key = stdscr.getch()
+        if key == ord('x'):
+            break
+
+wrapper(main)
